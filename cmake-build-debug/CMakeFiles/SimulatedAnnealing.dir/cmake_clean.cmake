5df5032a5a6194753f1619b1b412ef71c6091635
Jonathan Woolf <jlwoolf@mymail.mines.edu>
2021-01-12 10:27:17 -0700

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/SimulatedAnnealing.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimulatedAnnealing.dir/main.cpp.obj"
+  "SimulatedAnnealing.exe"
+  "SimulatedAnnealing.exe.manifest"
+  "SimulatedAnnealing.pdb"
+  "libSimulatedAnnealing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimulatedAnnealing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
